a3041a3fd2eccaf7731bd83c24ffdfb8dd982f2f
Tomer Nahum <github@tomer.nahum.org>
2023-06-29 10:11:42 -0400

    .

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -26,12 +26,22 @@
     
 <CreateRoomButton/>
 
-<!-- <br> -->
+<br>
 
-<h3 style="margin-top:.8rem"><u>Public Rooms:</u></h3>
+<h3 style="margin-top:.8rem"><u>Public Rooms:</u></h3> <p>(insecure since key is public)   </p>
+
 <a href="/chat/room/aec6ed16#3PPzzbuIjlMpo3b6WUs4LA">Public Room #1</a>
+<a href="/chat/room/b553d191#y2SunPQxcyqUeUC5Jaq3Tw">Public Room #2</a>
+<a href="/chat/room/ee7c3eaf#QHLpsxHrjGYmNeRYiTzf2A">Public Room #3</a>
 
-
+<style>
+    h3, p {
+        display:inline
+    }
+    a {
+        display: block
+    }
+</style>
 
 
  
