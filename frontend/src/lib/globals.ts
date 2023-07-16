e265643e1469e5eaf54d22b5976e8912ed1cafba
Tomer Nahum <github@tomer.nahum.org>
2023-07-16 12:24:35 -0400

    replaced env solution with .env

--- frontend/src/lib/globals.ts
+++ frontend/src/lib/globals.ts
@@ -1,11 +1,13 @@
-// export const serverUrl = "http://localhost:3000/"
-
-// export const urlBasePath = "localhost:5173"
-
+import { PUBLIC_SOCKET_SERVER_URL } from '$env/static/public';
 
 export type Url = `${"https"|"http"|"ws"}://${string}.${string}` | `http://localhost${string}`
 
 
+export const socketIoServerUrl =  PUBLIC_SOCKET_SERVER_URL as Url
+//Todo: may implement a way to change the backend url while the app is running
+
+
+/*
 //todo: maybe implement env vars besides vite.isProd
 const DEV_URL:Url = "http://localhost:3000/"
 const RAILWAY_SOCKET_IO_SERVER_URL:Url = "https://realtime-test-socketio-ttools.up.railway.app/"
@@ -16,8 +18,5 @@ if (import.meta.env.PROD)
 else
     socketIoServerUrl = DEV_URL
 export {socketIoServerUrl}
+*/
 
-// export function setApiUrl (newUrl:Url) {
-//     socketIoServerUrl = newUrl
-// } 
-// Not yet implemented
