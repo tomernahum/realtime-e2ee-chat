6869c21e6b3e4d1e8b49298ae35fc9fbbd5698bd
Tomer Nahum <github@tomer.nahum.org>
2023-07-16 12:01:12 -0400

    automatic env var thing

--- frontend/src/lib/globals.ts
+++ frontend/src/lib/globals.ts
@@ -6,10 +6,16 @@
 export type Url = `${"https"|"http"|"ws"}://${string}.${string}` | `http://localhost${string}`
 
 
+//todo: maybe implement env vars besides vite.isProd
 const DEV_URL:Url = "http://localhost:3000/"
 const RAILWAY_SOCKET_IO_SERVER_URL:Url = "https://realtime-test-socketio-ttools.up.railway.app/"
 
-export const socketIoServerUrl:Url = RAILWAY_SOCKET_IO_SERVER_URL //TODO env vars or something
+let socketIoServerUrl:Url;
+if (import.meta.env.PROD)
+    socketIoServerUrl = RAILWAY_SOCKET_IO_SERVER_URL 
+else
+    socketIoServerUrl = DEV_URL
+export {socketIoServerUrl}
 
 // export function setApiUrl (newUrl:Url) {
 //     socketIoServerUrl = newUrl
