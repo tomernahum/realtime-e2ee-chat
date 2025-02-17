0bcd23109de7f11eada833740a9d08c78bf07721
tomer <101682824+tomernahum@users.noreply.github.com>
2025-02-16 22:55:35 -0500

    attempt persister write

--- backend/src/persistence/persister.ts
+++ backend/src/persistence/persister.ts
@@ -5,6 +5,7 @@ import { DB_FILE_NAME } from '../../globals';
 
 import { drizzle } from 'drizzle-orm/libsql';
 import { encryptedMessages } from "./schema-sqlite";
+import { desc, eq } from "drizzle-orm";
 
 
 export interface Persister {
@@ -24,22 +25,76 @@ export class SqlitePersister implements Persister {
     }
 
     async saveMessage(roomId: string, message: EncryptedTextObj){
-        // await this.db.insert(encryptedMessages).values({
-        //     roomId: roomId,
-        //     cipher: message.cipher,
-        //     iv: message.iv,
-        // })
-        // return true
+
+        if (!roomId) {
+            console.error("NO ROOM ID");
+            return false;
+        }
+        const startTime = performance.now();
+        await this.db.insert(encryptedMessages).values({
+            roomId: roomId,
+            cipher: message.cipher,
+            iv: message.iv,
+        });
+        const endTime = performance.now();
+        console.log(`Insert Message call took ${endTime - startTime} milliseconds`);
+
+        
+        return true; //this is supposed to be if the opp was successful or not
     }
 
     async getMessages(roomId: string){
-        
+
+        // gets the most recent 1000 messages
+        //TODO: pagination
+
+
+        const startTime = performance.now();
+
+        const data = (
+          await this.db
+            .select()
+            .from(encryptedMessages)
+            .where(eq(encryptedMessages.roomId, roomId))
+            .orderBy(desc(encryptedMessages.id))
+            .limit(100)
+        ).reverse();
+
+        // clean and convert data into encrypted text objects
+        const clean_data = data.reduce(
+          (accumulator: EncryptedTextObj[], item) => {
+            //skip over corrupted items (todo maybe: set up a process for cleaning these rows)
+            if (!item.cipher || !item.iv) {
+                return accumulator;
+            }
+            const x = {
+                cipher: item.cipher,
+                iv: item.iv,
+            };
+            // accumulator
+            accumulator.push(x);
+            return accumulator;
+          },
+          []
+        );
+
+
+        const endTime = performance.now();
+        console.log(
+            `Select Messages From Chatroom Call took ${
+                endTime - startTime
+            } milliseconds`
+        );
+
+        //TODO: schedule removal of messages older than X most recent messages...
+        //this.cleanUpRoom();
+
+        //return cleaned data
+        return clean_data;
     }
 
     async ping() {
-        
-
-        
+        await this.db.select().from(encryptedMessages).limit(1);
     }
 
     // async randomSeed(){
