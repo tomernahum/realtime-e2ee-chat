87bd0ec725101045f477b49dd7e5da0c7cdb5a4c
tomer <101682824+tomernahum@users.noreply.github.com>
2025-02-17 00:02:13 -0500

    update frontend packages

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-auto';
 // TODO: consider changing to adapter cloudflare (remember to update readme at root)
@@ -7,7 +8,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
